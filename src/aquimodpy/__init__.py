166ed74d4f90d198970939201c539a826fd076d3
afc98 <58609280+afc98@users.noreply.github.com>
2026-05-17 16:40:50 +0100

    chore: add license, project configuration, and cleanup artifacts

--- src/aquimodpy/__init__.py
+++ src/aquimodpy/__init__.py
@@ -1,14 +1,45 @@
 from .Model import Model
 from .Runner import Runner
 from .EvaluationRunner import EvaluationRunner
-from .Components import SoilZone, UnsatZone, SatZone, Observations
+from .CalibrationRunner import CalibrationRunner
+from .Components import (
+    SoilZone,
+    UnsatZone,
+    SatZone,
+    Observations,
+    FAO,
+    NSSS,
+    SMAP,
+    Weibull,
+    Q3K3S1,
+    Q2K2S1,
+    Q1K1S1,
+    Q1T1S1,
+    VKD,
+    Q3K3S3,
+    Q2K2S2,
+    SA1D,
+)
 
 __all__ = [
     "Model",
     "Runner",
     "EvaluationRunner",
+    "CalibrationRunner",
     "SoilZone",
     "UnsatZone",
     "SatZone",
     "Observations",
+    "FAO",
+    "NSSS",
+    "SMAP",
+    "Weibull",
+    "Q3K3S1",
+    "Q2K2S1",
+    "Q1K1S1",
+    "Q1T1S1",
+    "VKD",
+    "Q3K3S3",
+    "Q2K2S2",
+    "SA1D",
 ]
